3eb9456dae400995acd8fecf2d1fdd010780ef96
Khalida Khalisha <b032010224@student.utem.edu.my>
2022-03-11 00:40:39 +0800

    Commit first

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class Location {
+
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
